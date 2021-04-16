62f54c15799ed44acdfd49ca5d1eb6a7106d973f
likhithpasupuleti <67852060+likhithpasupuleti@users.noreply.github.com>
2021-04-16 13:40:23 +0530

    Update Browser.java

--- src/main/java/com/calculatetripcost/baseclass/Browser.java
+++ src/main/java/com/calculatetripcost/baseclass/Browser.java
@@ -1,20 +1,13 @@
 package com.calculatetripcost.baseclass;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Properties;
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.Properties;
 
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.opera.OperaDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
@@ -33,20 +26,22 @@ public class Browser
 	//public RemoteWebDriver driver;
 	public ExtentReports report = ExtentReportManager.getReportInstance();
 	public ExtentTest logger;
-	public Properties prop=new Properties();
+	public Properties prop = new Properties();
 	public LandingPage landingpage;
 	public NairobiSearchResultsPage nairobi_Search_Results_Page;
 	public CruisesPage cruises_page;
 	public DeckPage deck_page;
 	
+	int flag=0;
 	//Execution of Testcases on local browser
 	public void invokeBrowser()
 	{
 		try
 		{
-			String browserName=property().getProperty("browser");
+			String browserName=property().getProperty("browser"); 				//Gets Browser Name from config.properties file
 			if(browserName.equalsIgnoreCase("Chrome"))
 			{
+				flag=1;
 				String driverPath = System.getProperty("user.dir") + "\\Drivers\\chromedriver.exe";
 				System.setProperty("webdriver.chrome.driver", driverPath);
 				driver = new ChromeDriver();
@@ -54,68 +49,53 @@ public class Browser
 				logger.log(Status.PASS,"Chrome Browser Successfully Launched");
 				RunScriptWriteExcel.write(3,6,"Pass");
 			}
-			else if(browserName.equalsIgnoreCase("edge"))
-			{
-				String driverPath = System.getProperty("user.dir") + "\\Drivers\\msedgedriver.exe";
-				System.setProperty("webdriver.edge.driver", driverPath);
-				driver = new EdgeDriver();
-				
-				logger.log(Status.PASS,"Edge Browser Successfully Launched");
-				RunScriptWriteExcel.write(4,6,"Pass");
-			}
 			else if(browserName.equalsIgnoreCase("firefox"))
 			{
+				flag=2;
+				
 				String driverPath = System.getProperty("user.dir") + "\\Drivers\\geckodriver.exe";
 				System.setProperty("webdriver.gecko.driver", driverPath);
 				driver =  new FirefoxDriver();
 				
 				logger.log(Status.PASS,"Fiefox Browser Successfully Launched");
+				RunScriptWriteExcel.write(4,6,"Pass");
 			}
-			else if(browserName.equalsIgnoreCase("opera"))
+			else if(browserName.equalsIgnoreCase("edge"))
 			{
-				String driverPath = System.getProperty("user.dir") + "\\Drivers\\operadriver.exe";
-				System.setProperty("webdriver.opera.driver", driverPath);
-				driver = new OperaDriver();
+				flag=3;
+						
+				String driverPath = System.getProperty("user.dir") + "\\Drivers\\msedgedriver.exe";
+				System.setProperty("webdriver.edge.driver", driverPath);
+				driver = new EdgeDriver();
 				
-				logger.log(Status.PASS,"Opera Browser Successfully Launched");
+				logger.log(Status.PASS,"Edge Browser Successfully Launched");
+				RunScriptWriteExcel.write(5,6,"Pass");
 			}
 			else
 			{
 				System.out.println("Invalid choice of browser");
-				RunScriptWriteExcel.write(5,6,"Pass");
 			}
 		}
 		catch(Exception e)
 		{
+			if(flag==1)
+			{
+				RunScriptWriteExcel.write(3,6,"Fail");
+			}
+			else if(flag==2)
+			{
+				RunScriptWriteExcel.write(4,6,"Fail");
+			}
+			else if(flag==3)
+			{
+				RunScriptWriteExcel.write(5,6,"Fail");
+			}
+			
 			logger.log(Status.FAIL,e.getMessage());
 		}
 	}
 	
-	//Execution of test  cases on Selenium Grid(i.e.,Parallel Execution)
-	public void remoteBrowser() throws MalformedURLException
-	{
-		try
-		{
-			//1.Define Desired capabilties
-			DesiredCapabilities cap=new DesiredCapabilities();
-			cap.setBrowserName("chrome");
-			cap.setPlatform(Platform.WINDOWS);
-				
-			//2.Chrome Options Definition
-			ChromeOptions options=new ChromeOptions();
-			options.merge(cap);
-				
-			//3.Hub url and RemoteWebdriver
-			String hubUrl="http://192.168.43.166:4444/wd/hub";   //Keeps on changing
-			driver=new RemoteWebDriver(new URL(hubUrl),options);
-		}
-		catch(Exception e)
-		{
-			logger.log(Status.FAIL,e.getMessage());
-		}
-	}
-	
-	//@AfterSuite
+	//Flush Reports
 	public void flushReports() 
 	{
 		report.flush();
@@ -134,7 +114,7 @@ public class Browser
 		{
 			Thread.sleep(seconds * 1000);
 		} 
-		catch (InterruptedException e) 
+		catch(Exception e) 
 		{
 			e.printStackTrace();
 		}
