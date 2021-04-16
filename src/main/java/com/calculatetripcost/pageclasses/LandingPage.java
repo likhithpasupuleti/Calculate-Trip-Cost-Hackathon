354754434444f81de95f9d5591b06ae308dcc28c
likhithpasupuleti <67852060+likhithpasupuleti@users.noreply.github.com>
2021-04-16 13:41:49 +0530

    Update LandingPage.java

--- src/main/java/com/calculatetripcost/pageclasses/LandingPage.java
+++ src/main/java/com/calculatetripcost/pageclasses/LandingPage.java
@@ -2,11 +2,10 @@ package com.calculatetripcost.pageclasses;
 
 import java.util.ArrayList;
 import java.util.List;
-import com.calculatetripcost.utils.RunScriptWriteExcel;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -14,6 +13,7 @@ import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 import com.calculatetripcost.baseclass.BaseClass;
 import com.calculatetripcost.utils.ReadUserDataFromExcel;
+import com.calculatetripcost.utils.RunScriptWriteExcel;
 
 public class LandingPage extends BaseClass
 {
@@ -31,22 +31,23 @@ public class LandingPage extends BaseClass
 	{
 		try
 		{
+			takeScreenshot("Trip Advisor Home Page");
 			logger.log(Status.INFO, "Clicking the Holiday Homes Button in Landing Page");
 			for(WebElement click_HolidayHomes:click_HolidayHomes_from_menu)
 			{
 				if(click_HolidayHomes.getText().contains("Holiday"))
 				{
-					click_HolidayHomes.click();
+					click_HolidayHomes.click();   
 					break;
 				}
 			}
 			reportPass("Holiday Homes Button Successfully Clicked in Landing Page");
-			RunScriptWriteExcel.write(6,6,"Pass"); 
+			RunScriptWriteExcel.write(6,6,"Pass");        //For writing test case Passed in Run Script WorkSheet
 		}
 		catch(Exception e)
 		{
 			reportFail(e.getMessage());
-			RunScriptWriteExcel.write(6,6,"Fail");  
+			RunScriptWriteExcel.write(6,6,"Fail");            //For writing test case Failed in Run Script WorkSheet
 		}
 	}
 	
@@ -58,17 +59,21 @@ public class LandingPage extends BaseClass
 	{
 		try
 		{
-			logger.log(Status.INFO,"Destination should entered in location input textbox");
+			takeScreenshot("Location Box");
 			
+			logger.log(Status.INFO,"Destination should entered in location input textbox");
 			String destination=ReadUserDataFromExcel.ReadDestination();   //Reads Nairobi Destination from Holiday_Homes_User_Details.xlsx
 			enter_destination.sendKeys(destination);
 			
+			takeScreenshot("Destination Entered");
 			reportPass("Destination is Entered in location input text box");
+			
 			RunScriptWriteExcel.write(7,6,"Pass");
 		}
 		catch(Exception e)
 		{
 			reportFail(e.getMessage());
+			
 			RunScriptWriteExcel.write(7,6,"Fail");
 		}
 	}
@@ -83,6 +88,8 @@ public class LandingPage extends BaseClass
 			List<WebElement> Search_Suggestions=new ArrayList<WebElement>(); 
 			Search_Suggestions = Parent.findElements(By.tagName("a"));
 			int temp=0;
+			
+			takeScreenshot("Auto Suggestions Displayed");
 			logger.log(Status.INFO,"Search Suggestions are displayed,Choose destination from the auto suggestion");
 			
 			//Here we check if the suggestions shown matches our desired suggestion
@@ -99,19 +106,22 @@ public class LandingPage extends BaseClass
 			//Navigating to the href that we got from Search Suggestions
 			driver.get(Search_Suggestions.get(temp).getAttribute("href"));
 			reportPass("Destination from the auto suggestion is clicked");
-			RunScriptWriteExcel.write(8,6,"Pass"); 
 			
+			takeScreenshot("Holiday Homes Results Page");
 			logger.log(Status.INFO, "Page Navigated to Nairobi Results Search Page");
+			RunScriptWriteExcel.write(8,6,"Pass"); 
+	
 			NairobiSearchResultsPage nsrp=new NairobiSearchResultsPage(driver,logger);
 			PageFactory.initElements(driver,nsrp);
 			return nsrp;
 		}
 		catch(Exception e) 
 		{
-			System.out.println("Search-Suggestions Not available");
-			driver.get("https://www.tripadvisor.in/VacationRentals-g294207-Reviews-Nairobi-Vacation_Rentals.html");
+			System.out.println("Provided Destination Search-Suggestions Not available");
+			driver.get("https://www.tripadvisor.in/VacationRentals-g294207-Reviews-Nairobi-Vacation_Rentals.html");  //If in auto-suggestion it is not displayed,then get this url to Open Nairobi Results Page
 			reportFail(e.getMessage());
-			RunScriptWriteExcel.write(8,6,"Pass"); 
+			
+			RunScriptWriteExcel.write(8,6,"Fail");
 		}
 		return null;
 	}
